067bc53c1ec0c68ceedde09734a936ae4b676d3d
Yong Sen <yongsen318@gmail.com>
2024-02-14 20:26:04 +0800

    Initial commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    //Firebase
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -33,6 +35,11 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+
+    buildFeatures{
+        dataBinding = true
+        viewBinding = true
+    }
 }
 
 dependencies {
@@ -41,7 +48,16 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.7.6")
+    implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
+
+
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+//Firebase
+    implementation(platform("com.google.firebase:firebase-bom:32.7.2"))
+    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-play-services:1.8.0-RC2")
+    implementation("com.google.firebase:firebase-firestore-ktx:24.10.0")
 }
--- app/src/main/java/com/example/finalyearproject/MainActivity.kt
+++ app/src/main/java/com/example/finalyearproject/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.example.finalyearproject
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.example.finalyearproject.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private val nav by lazy { supportFragmentManager.findFragmentById(R.id.navHost)!!.findNavController() }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setupActionBarWithNavController(nav)
     }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return nav.navigateUp() || super.onSupportNavigateUp()
+    }
+
 }
