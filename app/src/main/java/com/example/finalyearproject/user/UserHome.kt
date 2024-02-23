7ca11f70b2927668506496da94e2417ab4a4d70c
Yong Sen <yongsen318@gmail.com>
2024-02-23 21:04:30 +0800

    open file

---
+++ app/src/main/java/com/example/finalyearproject/user/UserHome.kt
@@ -0,0 +1,12 @@
+package com.example.finalyearproject.user
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.finalyearproject.R
+
+class UserHome : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user_home)
+    }
+}
